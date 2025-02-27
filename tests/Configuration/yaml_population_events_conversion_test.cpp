8b183c6e4e7f24e4c2692c735be46e8a48a07d56
KienTTran <kientt@temple.edu>
2025-02-26 21:33:45 -0500

    1. Running all person internal and population events and basic reporters 2. Working on specialist reporters.

--- tests/Configuration/yaml_population_events_conversion_test.cpp
+++ tests/Configuration/yaml_population_events_conversion_test.cpp
@@ -15,7 +15,7 @@ protected:
 
         // Setting up PopulationEvent
         PopulationEvents::PopulationEvent population_event;
-        population_event.set_name("introduce_parasites");
+        population_event.set_name("turn_on_mutation");
         population_event.set_info({event_info});
 
         // Adding event to PopulationEvents
@@ -27,22 +27,20 @@ protected:
 TEST_F(PopulationEventsTest, EncodePopulationEvents) {
     YAML::Node node = YAML::convert<PopulationEvents>::encode(population_events);
 
-    ASSERT_EQ(node["population_events"][0]["name"].as<std::string>(), "introduce_parasites");
+    ASSERT_EQ(node["population_events"][0]["name"].as<std::string>(), "turn_on_mutation");
 
     const auto& event_info_node = node["population_events"][0]["info"][0];
     EXPECT_EQ(event_info_node["date"].as<std::string>(), "2024/10/01");
-    EXPECT_EQ(event_info_node["location_id"].as<int>(), 1);
 }
 
 // Test decoding functionality
 TEST_F(PopulationEventsTest, DecodePopulationEvents) {
     YAML::Node node;
-    node["population_events"][0]["name"] = "introduce_parasites";
+    node["population_events"][0]["name"] = "turn_on_mutation";
     node["population_events"][0]["info"][0]["date"] = "2024/10/01";
-    node["population_events"][0]["info"][0]["location_id"] = 1;
 
     PopulationEvents decoded_population_events;
-    ASSERT_NO_THROW(YAML::convert<PopulationEvents>::decode(node, decoded_population_events));
+    ASSERT_NO_THROW(YAML::convert<PopulationEvents>::decode(node["population_events"], decoded_population_events));
 
     const auto& events = decoded_population_events.get_events_raw();
     ASSERT_EQ(events.size(), 1);
@@ -54,7 +52,7 @@ TEST_F(PopulationEventsTest, DecodePopulationEvents) {
 // Test missing fields during decoding
 TEST_F(PopulationEventsTest, DecodePopulationEventsMissingField) {
     YAML::Node node;
-    node["population_events"][0]["name"] = "introduce_parasites";  // Missing "info" field
+    node["population_events"][0]["name"] = "turn_on_mutation";  // Missing "info" field
 
     PopulationEvents decoded_population_events;
     EXPECT_THROW(YAML::convert<PopulationEvents>::decode(node, decoded_population_events), std::runtime_error);
